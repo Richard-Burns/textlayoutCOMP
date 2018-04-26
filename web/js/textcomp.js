7bfede021b08ee3dffb0f5a25c55db032cca7400
Richard Burns <richardburns@gmail.com>
2018-04-26 16:24:32 +0000

    added some more functionality
    still work to do before it gets to a "usable" state

--- web/js/textcomp.js
+++ web/js/textcomp.js
@@ -12,6 +12,10 @@ function setBackground(r,g,b,a) {
     $("body").css("background-color", 'rgba('+Math.round(r*255)+','+Math.round(g*255)+','+Math.round(b*255)+','+Math.round(a*255)+')');
 }
 
+function setHeader(r,g,b,a) {
+    $(".header").css("color", 'rgba('+Math.round(r*255)+','+Math.round(g*255)+','+Math.round(b*255)+','+Math.round(a*255)+')');
+}
+
 function enableColumns(toggle) {
     if (toggle == 'on') {
         $( "#maindiv" ).addClass( "twoColumns" );
